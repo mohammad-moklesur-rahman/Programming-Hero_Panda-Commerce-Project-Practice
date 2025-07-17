b3c60b3485fd6c7392351732371548720b3ea600
MOHAMMAD MOKLESUR RAHMAN <moklesur.se@gmail.com>
2025-07-18 00:20:24 +0600

    Add js file and header area navbar responsive for mobile devices

---
+++ js/panda.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "block";
+}
+
+function closeSidebar() {
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+}
